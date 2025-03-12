f1d9eb20c2edefad4866c59d996adf5aac700c71
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-03-12 23:34:46 +0900

    Title: 왔다 갔던 구역 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250312/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250312/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,33 +1,45 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int MAX_N = 100;
+        int MAX_R = 2000;
+        int OFFSET  = 1000;
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int OFFSET  = 1000;
-        int[] array = new int[2002];
 
-        
+        int[] x1 = new int[MAX_N];
+        int[] x2 = new int[MAX_N];
+        int[] array = new int[MAX_R+1];
+
+        int cur = 1000;
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
-            for(int j = 0; j < x-1;j++){
-                if(dir == 'R'){
-                OFFSET++;
-                }else{
-                OFFSET--;
-                }
-                array[OFFSET]++;
+            if(dir == 'R'){
+                x1[i] = cur;
+                x2[i] = cur + x;
+
+                cur += x;
+            }else{
+                x1[i] = cur - x;
+                x2[i] = cur; 
+
+                cur -= x;
             }
         }
 
         int cnt = 0;
 
-        for(int i = 0; i< 2001; i++){
-            if(array[i]>1){
-                cnt++;
-                System.out.println(i-1000);
+        for(int i = 0; i < N; i++){
+            for(int j = x1[i]; j < x2[i]; j++ ){
+                array[j]++;
             }
-                
+        }
+
+        for(int i = 0; i<MAX_R; i++) {
+            if(array[i]>=2)
+                cnt++;
         }
 
         System.out.println(cnt);
