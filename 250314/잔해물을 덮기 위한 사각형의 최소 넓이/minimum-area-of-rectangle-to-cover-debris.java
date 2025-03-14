1f750129def990d97477923c5bccf7b80249781c
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-03-14 16:17:12 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 143ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 250314/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = sc.nextInt();
+        int rect1_y1 = sc.nextInt();
+        int rect1_x2 = sc.nextInt();
+        int rect1_y2 = sc.nextInt();
+        int rect2_x1 = sc.nextInt();
+        int rect2_y1 = sc.nextInt();
+        int rect2_x2 = sc.nextInt();
+        int rect2_y2 = sc.nextInt();
+
+        int MAX = 2001;
+        int OFFSET = 1000;
+
+        int[][] square = new int[MAX][MAX];
+
+        for(int i = rect1_x1 + OFFSET; i < rect1_x2 + OFFSET; i++){
+            for(int j = rect1_y1 + OFFSET; j < rect1_y2 + OFFSET; j++){
+                square[i][j]++;
+            }
+        }
+
+        for(int i = rect2_x1 + OFFSET; i < rect2_x2 + OFFSET; i++){
+            for(int j = rect2_y1 + OFFSET; j < rect2_y2 + OFFSET; j++){
+                square[i][j]--;
+            }
+        }
+        int minX = MAX;
+        int maxX = 0;
+        int minY = MAX;
+        int maxY = 0;
+
+        for(int i = rect1_x1 + OFFSET; i <= rect1_x2 + OFFSET; i++){
+            for(int j = rect1_y1 + OFFSET; j <= rect1_y2 + OFFSET; j++){
+                if(square[i][j] == 1){
+                    minX = Math.min(minX,i - OFFSET);
+                    maxX = Math.max(maxX,i - OFFSET);
+                    minY = Math.min(minY,j - OFFSET);
+                    maxY = Math.max(maxY,j - OFFSET);
+                }
+            }
+        }
+
+
+        int ans = (maxX - minX + 1) * (maxY - minY + 1);
+        if (minX > maxX || minY > maxY) {
+            ans = 0;
+        }
+        System.out.println(ans);
+        // Please write your code here.
+    }
+}
