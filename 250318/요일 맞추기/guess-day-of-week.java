80b6acfae64b05a0848dbb87da5444c4fc1a8d07
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-03-18 18:33:43 +0900

    Title: 요일 맞추기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250318/요일 맞추기/guess-day-of-week.java
+++ 250318/요일 맞추기/guess-day-of-week.java
@@ -10,22 +10,48 @@ public class Main {
         String[] dayOfWeek = new String[]{"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
         int[] dayOfMonth = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
         int day = 0;
+        int dayOfFirst = d1;
+        int dayOfSecond = d2;
 
-        while(true){
+        for(int i = 0; i < m1; i++){
+            dayOfFirst += dayOfMonth[i];
+        }
+        for(int i = 0; i < m2; i++){
+            dayOfSecond += dayOfMonth[i];
+        }
+
+        if(dayOfFirst<dayOfSecond){
+            while(true){
             if(m1 == m2 && d1 == d2)
                 break;
             if( dayOfMonth[m1-1] == d1){
-
                 m1++;
                 d1 = 1;
             }
             day = day % 7;
             day++;
             d1++;
+            } 
         }
 
-        System.out.println(dayOfWeek[day]);
+        if(dayOfFirst>dayOfSecond){
+            day = (dayOfFirst - dayOfSecond) % 7;
+            day = 6 - day;
+            while(true){
+            if(m1 == m2 && d1 == d2)
+                break;
+            if( dayOfMonth[m2-1] == d2){
+                m2++;
+                d2 = 1;
+            }
+            day++;
+            d2++;
+            } 
+        }
 
+        
+        System.out.println(dayOfWeek[day]);
+        
 
     }
 }
