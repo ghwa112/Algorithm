777075489d7f34df622e87fa03df208bfc86e8e2
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-03-25 01:38:10 +0900

    Title: 1이 3개 이상 있는 위치, Time: 184ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 250325/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        int[] dx = new int[]{1,0,-1,0};
+        int[] dy = new int[]{0,-1,0,1};
+
+        int nx = 0;
+        int ny = 0;
+
+        int cnt = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for(int k = 0; k<4; k++){
+
+                    nx = i+dx[k];
+                    ny = j+dy[k];
+
+                    if(nx < 0 || nx > n-1 || ny < 0 || ny > n-1)
+                        continue;
+
+                    cnt += arr[nx][ny];
+                    
+                    if(cnt == 3)
+                        ans++;
+                }
+                cnt = 0;
+            }
+        }
+
+        System.out.println(ans);
+
+       
+    }
+}
