b8f887614f20398b183d635fc377165debfd9a40
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-04-03 16:12:52 +0900

    Title: 거울에 레이저 쏘기 2, Time: 160ms, Memory: 11MB, Status: Passed - Codetree

--- 250403/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 250403/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -13,37 +13,30 @@ public class Main {
 
         int dirNum = 0, x = 0, y = 0;
         int startX = 0, startY = 0;
-        int[][] startPoint = new int[n][n];
-        //find start point
-        for(int i = 0; i < (4*n); i++){
-            
-            startPoint[x][y] = i + 1;
-            if(i == startNum-1){
-                break;
-            }
-
-            int nx = x + dx[dirNum];
-            int ny = y + dy[dirNum];
-
-            if(!inRange(nx,ny,n)){
-                dirNum++;
-                continue;
-            }
-            x = nx;
-            y = ny;
-        }
+       
 
 
         //find start direction
-        if(startNum > 0 && startNum <= n){
+        if(startNum <= n){
             dirNum = 1;
+            startX = 0;
+            startY = startNum - 1;
         }else if(startNum > n && startNum <= (2*n)){
             dirNum = 2;
+            startX = startNum-n-1;
+            startY = n-1;
         }else if(startNum > (2*n) && startNum <= (3*n)){
             dirNum = 3;
+            startX = n-1;
+            startY = 3*n-startNum;
         }else{
             dirNum = 0;
+            startX = startY = 4*n-startNum;
+            startY = 0;
         }
+
+        x = startX;
+        y = startY;
 
         int ans = 0;
 
@@ -51,21 +44,15 @@ public class Main {
 
             ans++;
 
-            // System.out.print(dirNum+" ");
-            // System.out.println("("+x+" "+y+")");
-
             if(grid[x][y] == '/'){
-                dirNum = Math.abs(3-dirNum);
+                dirNum = 3-dirNum;
             }else{
                 dirNum = dirNum^1;
             }
 
-            // System.out.print(dirNum+" ");
-            // System.out.println("("+x+" "+y+")");
-
             int nx = x + dx[dirNum];
             int ny = y + dy[dirNum];
-            //System.out.println("("+nx+" "+ny+")");
+
             if(!inRange(nx,ny,n))
                 break;
             
