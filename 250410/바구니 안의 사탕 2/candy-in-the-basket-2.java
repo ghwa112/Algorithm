15000c8a106105a67c7819323e53e92f3a83f78b
신동화 <56102186+ghwa112@users.noreply.github.com>
2025-04-10 20:10:04 +0900

    Title: 바구니 안의 사탕 2, Time: 151ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250410/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int candies = 0;
+        int positions = 0;
+        int[] map = new int[100];
+        for (int i = 0; i < n; i++) {
+ 
+            candies = sc.nextInt();
+            positions = sc.nextInt();
+
+            map[positions] = candies; 
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i<100; i++){
+            int count = 0;
+            for(int j = i-k; j <= i+k; j++){
+                
+                
+                if(j>0 && j<100){
+                    count += map[j];
+                }
+            }
+            max = Math.max(max,count);
+        }
+        System.out.println(max);
+
+        
+    }
+}
